e0feba16e1b86c1f7d6cf4f83bda290421cfc434
AlanAlvesOliveira <alanalves1990@gmail.com>
2023-12-03 12:04:18 -0300

    'Formulario'

--- curso-alura/src/App.js
+++ curso-alura/src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 import Tabela from './Tabela';
+import Formulario from './Formulario';
 
 class App extends Component {
 
   state = {
-    autores :  [
+    autores: [
       {
         nome: 'Paulo',
         livro: 'React',
@@ -30,21 +31,35 @@ class App extends Component {
     ],
   }
 
-removeAutor = index => {
-
-  const { autores } = this.state;
-
+escutadorDeSubmit = autor =>{
   this.setState({
-    autores : autores.filter((autor, posAtual) => {
-      return posAtual !== index;
-    }),
+    //Spread operador
+    autores:[...this.state.autores, autor]
   })
-
 }
+
+  removeAutor = index => {
+
+    const { autores } = this.state;
+
+    this.setState({
+      autores: autores.filter((autor, posAtual) => {
+        return posAtual !== index;
+      }),
+    })
+
+  }
 
   render() {
     return (
-      <Tabela autores={this.state.autores} removeAutor = { this.removeAutor }/>  
+      <Fragment>
+      
+        <Formulario escutadorDeSubmit={this.escutadorDeSubmit}/>
+        <br/>
+        <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
+
+      </Fragment>
+
     );
   }
 }
